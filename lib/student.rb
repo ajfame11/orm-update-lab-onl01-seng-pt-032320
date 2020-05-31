01593a2a6400ead4f2f7bd7f14087889d19c29a9
aurel fontaine <ajfame11@gmail.com>
2020-05-31 22:47:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,8 +5,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 def initialize(id=nil, name, grade)
-  #  with DB[:conn]  	    @id, @name, @grade = id, name, grade
+  @id, @name, @grade = id, name, grade
 
-  end
+end
 
 end
